454e7780862b3075a6cd15d766dec7df11fac6bf
Samir Faci <samir@es.net>
2020-11-10 14:03:49 -0800

    WIP

--- compose/rabbit/rabbit_run.sh
+++ compose/rabbit/rabbit_run.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 set -em
 
-/usr/local/bin/docker-entrypoint.sh rabbitmq-server &
+##
+function main() {
+    /usr/local/bin/docker-entrypoint.sh rabbitmq-server &
 
-until nc -z -v -w30 localhost 15672; do
-    echo "Waiting 5 second until rabbit is coming up..."
-    sleep 5
-done
+    until nc -z -v -w30 localhost 15672; do
+        echo "Waiting 5 second until rabbit is coming up..."
+        sleep 5
+    done
 
-## Ensure the queues are created on start
-rabbitmqadmin declare queue name=${rabbitmq_input_key} durable=true
-rabbitmqadmin declare binding source=amq.direct destination=${rabbitmq_input_key} routing_key=${rabbitmq_input_key}
+    ## Ensure the queues are created on start
+    ## Sflow Queues
+    echo "Creating Queue for Sflow named: ${rabbitmq_input_sflow_key}"
+    rabbitmqadmin declare queue name=${rabbitmq_input_sflow_key} durable=true
+    rabbitmqadmin declare binding source=amq.direct destination=${rabbitmq_input_sflow_key} routing_key=${rabbitmq_input_sflow_key}
+    ## Netflow Queues
+    echo "Creating Queue for Netflow named: ${rabbitmq_input_netflow_key}"
+    rabbitmqadmin declare queue name=${rabbitmq_input_netflow_key} durable=true
+    rabbitmqadmin declare binding source=amq.direct destination=${rabbitmq_input_netflow_key} routing_key=${rabbitmq_input_netflow_key}
 
-fg %1
+    fg %1
+
+}
+
+main
